d0a3f7472e03edda7b4a3cbdde838898c0ecbe1e
DianeBada <92454613+DianeBada@users.noreply.github.com>
2023-06-14 17:04:58 +0200

    winning conidtion

--- LastOfThem/Assets/Scripts/Inventory/PCInventory.cs
+++ LastOfThem/Assets/Scripts/Inventory/PCInventory.cs
@@ -4,83 +4,89 @@ using UnityEngine;
 
 public class PCInventory : MonoBehaviour
 {
-
     public List<GameObject> playerInventory = new List<GameObject>();
     public List<GameObject> handInventory = new List<GameObject>();
-    
+
     InventoryUI inventoryUI;
-    GameObject Canvas;
+    GameObject canvas;
 
     private int maxCapacity = 10;
-    //private int maxHand = 1; //for now player can only have one obj in hand. Makes picking and dropping more intuitive for player
+    private int requiredTestTubes = 2; // Number of test tubes required for the win condition
 
     bool addToBag;
+    bool gameCompleted = false; // Tracks if the win condition has been achieved
 
     private void Start()
     {
-        Canvas = GameObject.FindGameObjectWithTag("Canvas");
-        inventoryUI = Canvas.GetComponent<InventoryUI>();
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        inventoryUI = canvas.GetComponent<InventoryUI>();
     }
 
     private void Update()
     {
-    
-        if(playerInventory.Count >= (maxCapacity))
+        if (playerInventory.Count >= maxCapacity)
         {
-
             addToBag = false;
             Debug.Log("Bag full");
-
-        } else{
+        }
+        else
+        {
             addToBag = true;
         }
 
-
-        //unequip
-        if(Input.GetKeyDown(KeyCode.L)) {
-            if(handInventory!=null)
+        // Unequip
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (handInventory.Count > 0)
             {
                 handInventory.RemoveAt(0);
-                //place object on ground
-            } else{
+                // Place object on the ground
+            }
+            else
+            {
                 Debug.Log("There is nothing in your hands to drop");
             }
+        }
+
+        // Check win condition
+        if (!gameCompleted && playerInventory.Count >= requiredTestTubes)
+        {
+            gameCompleted = true;
+            CompleteGame();
         }
     }
 
     public void moveObjToBag(GameObject obj)
     {
-
-        if(addToBag)
+        if (addToBag)
         {
-            //play pick up animation   
-                      
+            // Play pick up animation
             playerInventory.Add(handInventory[0]);
-            //change position of child so visible on screen         
-            handInventory[0].transform.SetParent(this.gameObject.transform);
+            // Change position of child to be visible on screen
+            handInventory[0].transform.SetParent(transform);
             handInventory.Clear();
 
-            if(obj.name =="TestTube")
+            if (obj.name == "TestTube")
             {
                 inventoryUI.updateTestTubeList();
             }
 
-            Debug.Log("playerInventory size: "+playerInventory.Count);
+            Debug.Log("Player inventory size: " + playerInventory.Count);
         }
-        else{
+        else
+        {
             handInventory.Clear();
-            Debug.Log("The bag is full, please remove an item"); //leaving object in had so player can drop
+            Debug.Log("The bag is full, please remove an item"); // Leaving object in hand so player can drop it
         }
-       
     }
 
     public void AddObjectToInventory(GameObject obj)
     {
         playerInventory.Add(obj);
-        
+
         if (handInventory.Count > 0)
         {
-            for (int i = 0; i <handInventory.Count-1; i++)
+            for (int i = 0; i < handInventory.Count - 1; i++)
             {
                 handInventory.RemoveAt(i);
                 handInventory[i].SetActive(false);
@@ -98,7 +104,11 @@ public class PCInventory : MonoBehaviour
         handInventory.Clear();
         obj.SetActive(false);
     }
-        
 
-    
+    void CompleteGame()
+    {
+        // Game completion logic
+        Debug.Log("Congratulations! You have completed the game.");
+        // Perform any other required actions for the win condition
+    }
 }
