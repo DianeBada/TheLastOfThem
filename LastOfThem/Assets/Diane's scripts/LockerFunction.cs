58e24e5117b3d76328388a1308599ad88aff1b4a
DianeBada <92454613+DianeBada@users.noreply.github.com>
2023-07-02 21:13:41 +0200

    prompt

--- LastOfThem/Assets/Diane's scripts/LockerFunction.cs
+++ LastOfThem/Assets/Diane's scripts/LockerFunction.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
@@ -9,7 +10,15 @@ public class LockerFunction : MonoBehaviour
     [SerializeField] private FirstPersonController firstPersonController;
     public GameObject indicator;
     public bool isOccupied = false;
+    public static GameObject[] lockers;
+    private GameObject closestLocker;
 
+
+    private void Awake()
+    {
+        lockers = GameObject.FindGameObjectsWithTag("locker");
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isOccupied)
@@ -35,6 +44,21 @@ public class LockerFunction : MonoBehaviour
 
     public void HidePlayer()
     {
+        firstPersonController.isInsideLocker = true;
+        closestLocker = GetClosestLocker();
+
+        if (closestLocker != null)
+        {
+            // Set the current locker as occupied by the player
+            isOccupied = true;
+            // Face the camera towards the locker's direction
+            Vector3 cameraDirection = closestLocker.transform.forward;
+            firstPersonController.m_Camera.transform.forward = cameraDirection;
+
+            // Place the camera in the center of the locker
+            Vector3 cameraPosition = closestLocker.transform.position;
+            firstPersonController.m_Camera.transform.position = cameraPosition;
+        }
         // Hide the player inside the locker
         // Disable player controls or restrict movement
         // Adjust camera view or provide a first-person view from inside the locker
@@ -43,6 +67,12 @@ public class LockerFunction : MonoBehaviour
 
     public void ExitLocker()
     {
+        firstPersonController.isInsideLocker = false;
+        isOccupied = false;
+
+        firstPersonController.m_Camera.transform.position = firstPersonController.m_OriginalCameraPosition;
+        firstPersonController.m_Camera.transform.rotation = firstPersonController.m_OriginalCameraRotation;
+
         // Allow the player to exit the locker
         // Enable player controls or restore movement
         // Adjust camera view back to normal
@@ -52,5 +82,30 @@ public class LockerFunction : MonoBehaviour
     public void DisplayPrompt()
     {
         indicator.SetActive(true);
+        Debug.Log("indicator is active");
+    }
+
+    private GameObject GetClosestLocker()
+    {
+        GameObject closestLocker = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 playerPosition = firstPersonController.transform.position;
+        Vector3 playerDirection = firstPersonController.transform.forward;
+
+        foreach (GameObject locker in lockers)
+        {
+            // Calculate the direction from the player to the locker
+            Vector3 lockerDirection = locker.transform.position - playerPosition;
+            float distance = Vector3.Distance(locker.transform.position, playerPosition);
+
+            // Check if the locker is in front of the player and closer than the current closest locker
+            if (Vector3.Dot(playerDirection, lockerDirection) > 0 && distance < closestDistance)
+            {
+                closestLocker = locker;
+                closestDistance = distance;
+            }
+        }
+
+        return closestLocker;
     }
 }
